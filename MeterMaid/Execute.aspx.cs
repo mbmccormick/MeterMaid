12a5c55880a443769196c37330e01a1e2f0fa0c1
Matt McCormick <mbmccormick@gmail.com>
2011-09-30 10:13:30 -0400

    Initial commit.

---
+++ MeterMaid/Execute.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TwilioRest;
+using System.Configuration;
+using System.Collections;
+
+namespace MeterMaid
+{
+    public partial class Execute : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DatabaseDataContext db = new DatabaseDataContext();
+
+            Account account = new Account(ConfigurationManager.AppSettings["TwilioAccountSid"], ConfigurationManager.AppSettings["TwilioAuthToken"]);
+
+            foreach (Reminder r in db.Reminders.Where(z => z.DueTime <= DateTime.UtcNow))
+            {
+                Hashtable data = new Hashtable();
+                data.Add("To", r.PhoneNumber);
+                data.Add("From", ConfigurationManager.AppSettings["TwilioNumber"]);
+                data.Add("Body", "Your parking meter will expire in 15 minutes.");
+
+                account.request(string.Format("/2010-04-01/Accounts/{0}/SMS/Messages", ConfigurationManager.AppSettings["TwilioAccountSid"]), "POST", data);
+
+                db.Reminders.DeleteOnSubmit(r);
+            }
+
+            db.SubmitChanges();
+        }
+    }
+}
---
+++ MeterMaid/Receive.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MeterMaid
+{
+    public partial class Receive : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DatabaseDataContext db = new DatabaseDataContext();
+
+            Reminder data = new Reminder();
+
+            data.ReminderID = Guid.NewGuid();
+            data.PhoneNumber = Request["From"];
+            data.DueTime = ParseDueTime(Request["Body"]);
+            data.CreatedDate = DateTime.UtcNow;
+
+            db.Reminders.InsertOnSubmit(data);
+
+            db.SubmitChanges();
+        }
+
+        private DateTime ParseDueTime(string value)
+        {
+            int duration = Convert.ToInt32(value.Split(Convert.ToChar(" "))[0]);
+            string unit = value.Split(Convert.ToChar(" "))[1];
+
+            DateTime result = DateTime.UtcNow;
+
+            switch (unit)
+            {
+                case "minutes":
+                    result = result.AddMinutes(duration);
+                    break;
+                case "hours":
+                    result = result.AddHours(duration);
+                    break;
+                default:
+                    throw new Exception("Invalid DateTime format for '" + value + "'.");
+            }
+
+            return result;
+        }
+    }
+}
