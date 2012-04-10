37a3856fdd756ce7880149de2118f4e06e97e9f4
Matt McCormick <mbmccormick@gmail.com>
2012-04-10 11:40:59 -0400

    Adding background worker.

---
+++ BackgroundWorker/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net;
+
+namespace BackgroundWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                WebClient client = new WebClient();
+                client.DownloadString("http://metermaid.apphb.com/execute.aspx");
+
+                Thread.Sleep(60 * 1000);
+            }
+        }
+    }
+}
